c0bafe0a71e05817848ed57ceaafb7ebca454821
gebrie-dev <151966244+gebrie-dev@users.noreply.github.com>
2024-05-18 05:30:05 -0400

    Add files via upload

---
+++ solidty/codebases/Address_Interactions/Calling Contracts/sidekick.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IHero {
+    function alert() external;
+}
+
+contract Sidekick {
+    function sendAlert(address hero) external {
+        // TODO: alert the hero using the IHero interface
+    }
+}
