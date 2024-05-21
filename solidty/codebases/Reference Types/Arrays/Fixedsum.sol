770c6cf4e3aacde8e410955509c4e6554a909d57
Deborah <123468697+di17-z@users.noreply.github.com>
2024-05-21 17:19:51 +0300

    Create Fixedsum.sol

---
+++ solidty/codebases/Reference Types/Arrays/Fixedsum.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Contract {
+    function sum(uint[5] memory numbers) external pure returns (uint) {
+        uint total = 0;
+        for (uint i = 0; i < numbers.length; i++) {
+            total += numbers[i];
+        }
+        return total;
+    }
+    
+}
