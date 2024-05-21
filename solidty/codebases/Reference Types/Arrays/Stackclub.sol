52bf25bb07a7f062d81fe56eaaf54c9be8aa11f3
Deborah <123468697+di17-z@users.noreply.github.com>
2024-05-21 19:32:34 +0300

    Update Stackclub.sol

--- solidty/codebases/Reference Types/Arrays/Stackclub.sol
+++ solidty/codebases/Reference Types/Arrays/Stackclub.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+contract StackClub {
+    address[] public members;
+    mapping(address => bool) public isExistingMember;
+
+    constructor() {
+        members.push(msg.sender);
+        isExistingMember[msg.sender] = true;
+    }
+
+    function addMember(address newMember) external {
+        require(isExistingMember[msg.sender], "Only existing members can add new members");
+        members.push(newMember);
+        isExistingMember[newMember] = true;
+    }
+
+    function removeLastMember() external {
+        require(isExistingMember[msg.sender], "Only existing members can remove members");
+        require(members.length > 1, "Cannot remove the last member");
+        
+        address lastMember = members[members.length - 1];
+        members.pop();
+        isExistingMember[lastMember] = false;
+    }
+
+    function isMember(address memberAddress) public view returns (bool) {
+        return isExistingMember[memberAddress];
+    }
+}
