3025b479ae1653a6060606a8ab61f8a0af1af1d9
Natnael Girma <117731940+NatiGirma@users.noreply.github.com>
2024-05-21 10:03:17 -0900

    Update BaseContracts.sol

--- solidty/codebases/Applied solidity/inheritance/BaseContracts.sol
+++ solidty/codebases/Applied solidity/inheritance/BaseContracts.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Ownable {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+}
+
+contract Transferable is Ownable {
+    function transfer(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "New owner cannot be the zero address");
+        owner = newOwner;
+    }
+}
 
